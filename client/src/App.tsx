4c71b53e7e2d5872b987f10990419122b76c4a32
Temitope Olawuni <olawunitemitope68@gmail.com>
2024-06-18 15:07:00 +0100

    add router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,47 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
 
 
-const App = () => {
-  return (
-    <div className="text-red-500">App</div>
-  )
+
+import About from "./pages/About"
+import Home from "./pages/Home"
+import SIgnUp from "./pages/SIgnOut"
+import SignIn from "./pages/SignIn"
+import React from "react";
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+     element:<Home />,
+  },
+  {
+    path:'/sign-in',
+     element:<SignIn />
+  },
+  {
+    path:'/sign-up',
+     element:<SIgnUp />
+  },
+  {
+    path:'/about',
+     element:<About />
+  },
+  {
+   
+  },
+]);
+
+
+function App() {
+
+  return ( 
+    <React.StrictMode>
+      <RouterProvider router={router}/>
+    </React.StrictMode>
+   )
 }
 
 export default App
---
+++ client/src/pages/SIgnOut.tsx
@@ -0,0 +1,9 @@
+
+
+const SIgnUp = () => {
+  return (
+    <div>SIgnOut</div>
+  )
+}
+
+export default SIgnUp
